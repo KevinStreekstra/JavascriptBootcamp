4697bf7cf831ec9a761d38443ca79a36dd2244ed
KevinStreekstra <kevinstreekstra07@gmail.com>
2017-09-20 08:34:07 +0200

    Add files via upload

---
+++ opdracht1.js
@@ -0,0 +1,40 @@
+var variableone = 1;
+var variabletwo = 2;
+var variablethree = 3;
+var variablefour = 4;
+var variablefive = 5;
+var variablesix = 6;
+var variableseven = 7;
+var variableeight = 8;
+var variablenine = 9;
+var variableten = 10;
+var variableeleven = 11;
+var variabletwelve = 12;
+var variablethirteen = 13;
+var variablefourteen = 14;
+var variablefiveteen = 15;
+var variablesixteen = true;
+var variableseventeen = "Bart RechterBerg";
+
+variableone = "one";
+variabletwo = "two";
+variabletwelve = "12";
+
+
+console.log(one);
+
+console.log(typeof(one));
+console.log(variablethree + variablefour);
+console.log(variablefive + two);
+console.log(variableeleven + variabletwelve);
+console.log(variablesixteen + variablefiveteen);
+console.log(variablefiveteen + variabletwelve);
+console.log(typeof(two));
+
+function VAnaam() {
+  var vn1 = Bart;
+  var achter1 = Middelberg;
+  console.log(vn1 + achter1);
+}
+
+console.log(548121600 + );
